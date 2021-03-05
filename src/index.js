2552f00d09c627d449b9b3d4529ef6c10dfaa3fc
Alex080498 <sasha.kanischewa@yandex.ru>
2021-03-05 22:26:07 +0300

    feat:solve brackets problem

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+  for (let element of str) {
+    if (element=='(') {
+      stack.push(element);
+    }
+    if (element ==')') {
+      stack.pop(element);
+    }
+    if (element=='{') {
+      stack.push(element);
+    }
+    if (element =='}') {
+      stack.pop(element);
+    }
+    if (element=='|') {
+      stack.push(element);
+    }
+    if (element =='|') {
+      stack.pop(element);
+    }
+    if (element=='[') {
+      stack.push(element);
+    }
+    if (element ==']') {
+      stack.pop(element);
+    }
+  }
+if (stack.length==0) {
+return true;
+} else {
+  return false;}
 }
+
+
+  
